c95c124a59e2fe2e1514b975c33aed1ee7669dd1
Yuanhui Fang <Yuanhui Fang>
2015-12-16 19:31:45 +0800

    组合版test

--- CM1.hs
+++ CM1.hs
@@ -4,39 +4,48 @@ module Main where
 
 import Graphics.UI.WX
 import Graphics.UI.WXCore
-
+import Data.Matrix
+import Status
+import CXM
+import System.IO as S
+import Data.ByteString as B
 
 main :: IO()
-main
- = run gui
+main = run gui
  
 gui :: IO()
-gui
-  =  do -- create top frame
-       f <- frameCreateTopFrame "CrossMe"
-       q <- button f [ text := "quit" , on command := close f ]
-       h <- button f [ text := "help" , on command := chelp f ]
-       a <- button f [ text := "about", on command := about f ]
-       --s <- scrolledWindowCreate f idAny rectNull (wxHSCROLL + wxVSCROLL + wxNO_FULL_REPAINT_ON_RESIZE + wxCLIP_CHILDREN)
-       p <- panel  f [ clientSize := sz 400 300 ]
-       -- virtual size is 20*40 = 800 pixels
-       --scrolledWindowSetScrollbars s 20 20 40 40 0 0 False
-       
-       set f [ layout := column 0
-                         [ fill $ widget p
-                         , hfloatCentre $ margin 5 $ row 5 [widget q, widget h, widget a]
-                         ]
-             , defaultButton := q
-             ]
-             
-       vballs <- varCreate []
-       windowOnPaint p (paintBalls vballs)
+gui =  do -- create top frame
+    f <- frameCreateTopFrame "CrossMe"
+    q <- button f [ text := "quit" , on command := close f ]
+    h <- button f [ text := "help" , on command := chelp f ]
+    a <- button f [ text := "about", on command := about f ]
+    --s <- scrolledWindowCreate f idAny rectNull (wxHSCROLL + wxVSCROLL + wxNO_FULL_REPAINT_ON_RESIZE + wxCLIP_CHILDREN)
+    p <- panel  f [ clientSize := sz 400 300 ]
+    -- virtual size is 20*40 = 800 pixels
+    --scrolledWindowSetScrollbars s 20 20 40 40 0 0 False
 
-       -- left-click: new ball, right-click: new window
-       windowOnMouse p False {- no motion events -} (onMouse1 p vballs)
-       windowShow f
-       windowRaise f
-       return ()
+    set f [ layout := column 0
+                        [ fill $ widget p
+                        , hfloatCentre $ margin 5 $ row 5 [widget q, widget h, widget a]
+                        ]
+            , defaultButton := q
+            ]
+
+    varC <- varCreate initChessBoard
+    cb <- testOfLoad
+    case cb of
+        Nothing -> return (Nothing)
+        Just c -> do
+            varUpdate varC(\old -> loadChessBoard c)
+
+
+    windowOnPaint p (paintBalls varC)
+
+    -- left-click: new ball, right-click: new window
+    windowOnMouse p False {- no motion events -} (onMouse1 p varC)
+    windowShow f
+    windowRaise f
+    return ()
 
 onMouse1 w vballs mouse
       = case mouse of
@@ -45,23 +54,30 @@ onMouse1 w vballs mouse
           other                  -> skipCurrentEvent      -- unprocessed event: send up the window chain
 
     -- add a new ball
-dropBall w vballs pt
-      = do varUpdate vballs ([ pt]:)
+dropBall w varC pt
+      = do varUpdate varC (switchLocation (rownum pt) (colnum pt))
            windowRefresh w False
 
     -- paint the balls
-paintBalls vballs dc  viewRect --updateAreas
+paintBalls varC dc  viewRect --updateAreas
       = withBrushStyle (BrushStyle BrushSolid white)  $ \brushWhite ->
         --withPenStyle (penColored blue 5) $ \penMedBlue ->
         do -- dcClearRect dc view
            dcSetBrush dc brushWhite
            dcDrawRectangle dc (rect (pt 20 20) (sz 500 500))
-           drawgrid dc (1, (Rect 80 80 20 20))
-           drawgrid2 dc ([1,0,-1,1,1] ,(Rect 100 100 20 20))
-           drawgrid3 dc [[1,1,0,-1,-1],[0,1,-1,1,0],[0,0,-1,1,1]](Rect 120 120 20 20)
-           drawLine2 dc (pt 80 80)10 5
-           drawLine2_ dc (pt 80 80)10 5
-           writeNum3 dc (pt 85 85) [[1,2,3,0,5],[2,1,2,3,1],[2,3,4,9,0,5]]
+           --drawgrid dc (1, (Rect 80 80 20 20))
+           --drawgrid2 dc ([1,0,-1,1,1] ,(Rect 100 100 20 20))
+           user<-VarGet varC
+           drawgrid3 dc ((toLists . userMosaic) user)(Rect 80 80 20 20)
+           drawLine2 dc (pt 80 80) cols rows
+           drawLine2_ dc (pt 80 80)cols rows
+            writeNum3 dc (pt 85 newY) ((toLists . vHeader) user)
+            writeNum3 dc (pt newX 85) ((toLists . hHeader) user)
+            where
+                newY = 85 + 20 * ((nrows . hHeader) user)
+                newX = 85 + 20 * ((ncols . vHeader) user)
+                cols = ((nrows . hHeader) user)+ ((ncols . vHeader) user)
+                rows =  ((nrows . vHeader) user)+ ((ncols . hHeader) user)
        
 --horizontal--         
 drawLine::DC()->Int->Point->IO()
@@ -99,7 +115,27 @@ drawgrid2 dc (m, (Rect x y w h))=mapM_(drawgrid  dc)[(a, (Rect (x+20*b) y 20 20)
 --drawgird3
 drawgrid3 dc mm (Rect x y w h)=mapM_(drawgrid2 dc)[(a,(Rect x (y+20*b)20 20))|(a,b)<-zip mm [0..(length(mm)-1)]]
 
+---------------------------------------------------------------------
+newpt::Point->Point
+newpt (Point x y) = (pt  (x-(x `mod` 20))(y-(y `mod` 20)))    
 --------------------------------------------------------------------
+rownum::Point->Int->Int
+rownum (Point x y) toprow =((y-80)`div` 20 )-toprow+1
+
+colnum::Point->Int->Int
+colnum (Point x y) topcol = ((x-80)`div` 20)-topcol+1
+
+--------------------------------------------------------------------
+testOfLoad = do
+    inh <- S.openBinaryFile "u.cxm" ReadMode
+    instr <- B.hGetContents inh
+    hClose inh
+    case parse parseCMX instr of
+        Right c -> return (Just( loadChessBoard c))
+        Left err -> do
+            S.putStrLn err
+            return (Nothing)
+---------------------------------------------------------------------           
 about :: Window a -> IO ()
 about w
   = infoDialog w "About Camels" "Camels\n\nby Maarten Löfler\nmloffler@cs.uu.nl\n\nCamels was written using wxHaskell"
